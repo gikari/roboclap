ee34adecf28db425263b833069ead8f68b293736
Mikhail Zolotukhin <mail@genda.life>
2021-06-14 21:50:29 +0300

    fix: formatting

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ fn main() {
     let config = roboclap::Config::from_args();
 
     if let Err(e) = roboclap::run(config) {
-        eprintln!("Error occurred: {}",
-                  e);
+        eprintln!("Error occurred: {}", e);
         std::process::exit(1);
     }
 }
